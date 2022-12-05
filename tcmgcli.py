272e2f45ce2363072e9cfa3e34486f29f7e712a9
Michael Silva <99063625+mrubensilva@users.noreply.github.com>
2022-12-05 15:54:11 -0600

    Add argparse, Add md5 command

---
+++ tcmgcli.py
@@ -0,0 +1,21 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser(description='Access each endpoint in the REST API belonging to TCMG 412 Group 3.', usage='''tcmgcli.py COMMAND <input>\n
+commands:
+  md5          Return the MD5 hash of the string input
+  factorial    Return the factorial of the integer input
+  fibonacci    Return an array of Fibonacci numbers <= the integer input
+  is-prime     Return a boolean to decide if the integer input is prime
+  slack-alert
+''')
+parser.add_argument('COMMAND', help = 'Subcommand to run (see "commands:")')
+parser.add_argument('input', help = 'String or integer argument')
+args = parser.parse_args()
+
+if args.COMMAND == 'md5':
+  url = 'http://35.208.233.80/md5/' + args.input
+  x = requests.get(url)
+  jsonResponse = x.json()
+  output = jsonResponse['output']
+  print(output)
