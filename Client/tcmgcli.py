63b8eb8145f1764a2a23eaa2a65f79e2aece7642
Michael Silva <99063625+mrubensilva@users.noreply.github.com>
2022-12-05 22:45:36 -0600

    Add handling for unrecognized commands

--- Client/tcmgcli.py
+++ Client/tcmgcli.py
@@ -1,17 +1,32 @@
 import argparse
 import requests
 
+cmd_list = {
+  'md5': 'Return the MD5 hash of the string input',
+  'factorial': 'Return the factorial of the integer input',
+  'fibonacci': 'Return an array of Fibonacci numbers <= the integer input',
+  'is-prime': 'Return a boolean to decide if the integer input is prime',
+  'slack-alert': 'Post input into Slack',
+  'redis': 'Use Redis database [INTERACTIVE, no input required]'
+}
+
 parser = argparse.ArgumentParser(description='Access each endpoint in the REST API belonging to TCMG 412 Group 3.', usage='''tcmgcli.py COMMAND <input>\n
 commands:
   md5          Return the MD5 hash of the string input
   factorial    Return the factorial of the integer input
   fibonacci    Return an array of Fibonacci numbers <= the integer input
   is-prime     Return a boolean to decide if the integer input is prime
-  slack-alert
+  slack-alert  Post input into Slack
+  redis        Use Redis database [INTERACTIVE, no input required]
 ''')
 parser.add_argument('COMMAND', help = 'Subcommand to run (see "commands:")')
 parser.add_argument('input', help = 'String or integer argument')
 args = parser.parse_args()
+
+if args.COMMAND not in cmd_list:
+  print("Unrecognized command")
+  parser.print_help()
+  exit(1)
 
 if args.COMMAND == 'md5':
   url = 'http://35.208.233.80/md5/' + args.input
@@ -52,3 +67,10 @@ if args.COMMAND == 'is-prime':
     print(output)
   except:
       print('Must use an integer for <input>')
+
+if args.COMMAND == 'slack-alert':
+  url = 'http://35.208.233.80/slack-alert/' + args.input
+  x = requests.get(url)
+  jsonResponse = x.json()
+  output = jsonResponse['output']
+  print(output)
